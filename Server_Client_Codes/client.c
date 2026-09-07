0a54433d500b68dbdb8e66a100166b2195aa4bf8
Islam Dabbour <islamdabbour25@gmail.com>
2026-09-07 21:14:27 +0300

    Making the UI for the clinet code

--- Server_Client_Codes/client.c
+++ Server_Client_Codes/client.c
@@ -7,9 +7,45 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
+struct Matrix{
+
+    int rows;
+    int columns;
+    int *arr;
+};
+
 void error(const char *msg){
     perror(msg);
     exit(0);
+}
+
+struct Matrix fillMatrix(struct Matrix *matrix){
+
+    for (int i = 0; i < matrix->rows; i++) {
+
+        for (int j = 0; j < matrix->columns; j++) {
+
+            matrix->arr[i * matrix->columns + j] = (rand() % 100) + 1; 
+
+        }
+
+    }
+
+    return *matrix;
+
+}
+
+struct Matrix createMatrix(int rows, int columns){
+
+    
+    struct Matrix matrix = {
+        .rows = rows,
+        .columns = columns,
+        .arr = calloc((size_t)rows * columns, sizeof *matrix.arr)
+    };
+
+    return matrix;
+
 }
 
 int main(int argc, char **argv){
@@ -35,6 +71,9 @@ int main(int argc, char **argv){
     }
 
     printf("[+] Client socket created.\n");
+    printf("=================\n");
+    printf("=   Client  UI  =\n");
+    printf("=================\n");
 
     memset(&addr, '\0', sizeof(addr));
 
@@ -50,6 +89,28 @@ int main(int argc, char **argv){
     }
 
     printf("[+] Connected to server.\n"); //senario
+
+    printf("=> Enter the number of rows for matrix A: \n");
+    printf("=> ");
+    int rowsA, columnsB, columnsA, rowsB;
+
+    scanf("%d",&rowsA);
+
+    printf("=> Enter the number of columns for matrix A/ rows for matrix B:\n");
+    printf("=> ");
+
+    scanf("%d",&columnsA);
+    rowsB = columnsA;
+
+    printf("=> Enter the number of columns for matrix B: \n");
+    printf("=> ");
+    scanf("%d",&columnsB);
+
+    struct Matrix matrixA = createMatrix(rowsA, columnsA);
+    struct Matrix matrixB = createMatrix(rowsB, columnsB);
+    fillMatrix(&matrixA);
+    fillMatrix(&matrixB);
+
 
 
 
