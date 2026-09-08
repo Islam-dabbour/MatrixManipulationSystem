1268090dfbde4fc57a96e7207e937d3a15bdf9b4
Islam Dabbour <islamdabbour25@gmail.com>
2026-09-08 12:25:17 +0300

    Improving The Server Code to handle multiple clients concurrntly

--- Server_Client_Codes/server.c
+++ Server_Client_Codes/server.c
@@ -6,11 +6,34 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <time.h>
 
 void error(const char *msg){
     perror(msg);
-    exit(0);
+    exit(EXIT_FAILURE);
+}
+
+void *handle_client(void *socket_pointer){
+    int client_sock = *(int *)socket_pointer;
+    char buffer[1024];
+    ssize_t bytes_received;
+
+    free(socket_pointer);
+    printf("[+] Client connected.\n");
+
+    /* Keep the connection owned by this thread until the client disconnects. */
+    while((bytes_received = recv(client_sock, buffer, sizeof(buffer), 0)) > 0){
+        (void)bytes_received;
+    }
+
+    if(bytes_received < 0){
+        perror("Receive Error");
+    }
+
+    close(client_sock);
+    printf("[-] Client disconnected.\n");
+    return NULL;
 }
 
 int main(int argc, char **argv){
@@ -22,11 +45,10 @@ int main(int argc, char **argv){
 
     int port = atoi(argv[1]);
 
-    int server_sock, client_sock;
+    int server_sock;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_size;
-
-    char buffer[1024];
+    int reuse_address = 1;
 
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -36,6 +58,12 @@ int main(int argc, char **argv){
     }
 
     printf("[+] Server socket created.\n");
+
+    if(setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR,
+                  &reuse_address, sizeof(reuse_address)) < 0)
+    {
+        error("Setsockopt Error");
+    }
 
     memset(&server_addr, '\0', sizeof(server_addr));
 
@@ -53,24 +81,40 @@ int main(int argc, char **argv){
 
     printf("[+] Bound to port %d\n", port);
 
-    listen(server_sock, 5);
+    if(listen(server_sock, 5) < 0){
+        error("Listen Error");
+    }
 
-    printf("Listening...\n");
+    printf("Listening. Press Ctrl+C to stop the server.\n");
 
-    addr_size = sizeof(client_addr);
+    while(1){
+        pthread_t client_thread;
+        int *client_sock = malloc(sizeof(*client_sock));
 
-    client_sock = accept(server_sock,(struct sockaddr*)&client_addr,&addr_size);//the senario
+        if(client_sock == NULL){
+            perror("Memory Allocation Error");
+            continue;
+        }
 
-	
+        addr_size = sizeof(client_addr);
+        *client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_size);
 
-	if(client_sock < 0)
-        error("Accept Error");
+        if(*client_sock < 0){
+            perror("Accept Error");
+            free(client_sock);
+            continue;
+        }
 
-    printf("[+] Client Connected.\n");
-    
-    close(client_sock);
-	close(server_sock);
+        if(pthread_create(&client_thread, NULL, handle_client, client_sock) != 0){
+            perror("Thread Creation Error");
+            close(*client_sock);
+            free(client_sock);
+            continue;
+        }
 
-    return 0;
+        pthread_detach(client_thread);
+    }
 
+    close(server_sock);
+    return EXIT_SUCCESS;
  }
